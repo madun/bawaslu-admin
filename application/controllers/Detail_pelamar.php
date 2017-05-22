3014e35043efcbd0df9075e84dd0e49d81d7cae3
ilman@niagateknologi.net <ilman@niagateknologi.net>
2017-05-22 13:04:44 +0700

    new

--- application/controllers/Detail_pelamar.php
+++ application/controllers/Detail_pelamar.php
@@ -3,6 +3,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Detail_pelamar extends CI_Controller {
 
+	public function __construct(){
+		parent::__construct();
+		$this->load->model('model_detailPelamar');
+	}
+
 	public function index()
 	{
 		$this->model_security->getsecurity();
@@ -13,6 +18,12 @@ class Detail_pelamar extends CI_Controller {
 		$isikonten['sub_judul'] = ''; //untuk $judul dan $sub_judul yg ada di tampilan_home.php (breadcrumb)
     // breadcrumb
 		$this->load->view('body', $isikonten);
+	}
+
+	public function getData(){
+		$this->model_security->getsecurity();
+		$result = $this->model_detailPelamar->getData();
+		echo json_encode($result);
 	}
 
 
