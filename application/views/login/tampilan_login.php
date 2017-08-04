2cec00ae43ea3f4c0a995c7c02265485ede20348
madun <ilmankori@gmail.com>
2017-08-04 15:05:17 +0700

    ini

--- application/views/login/tampilan_login.php
+++ application/views/login/tampilan_login.php
@@ -2,7 +2,7 @@
 
 <head>
   <meta charset="utf-8">
-  <title>BAWASLU I</title>
+  <title>BAWASLU</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <link rel="stylesheet" href="<?php echo base_url(); ?>assets/materialize/css/materialize.min.css">
   <link rel="stylesheet" href="<?php echo base_url(); ?>assets/custome.css">
